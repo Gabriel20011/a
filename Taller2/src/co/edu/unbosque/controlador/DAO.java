5746824e88a1a2f264402588a5894bed7b7959c4
gabriel <gortega2001@gmail.com>
2020-04-07 17:10:28 -0500

    ñ

--- Taller2/src/co/edu/unbosque/controlador/DAO.java
+++ Taller2/src/co/edu/unbosque/controlador/DAO.java
@@ -3,14 +3,14 @@ package co.edu.unbosque.controlador;
 import java.util.ArrayList;
 
 public class DAO {
-	
+
 	private ArrayList<String> nombres;
 	private ArrayList<String> apellidos;
 	private ArrayList<String> edades;
 	private ArrayList<String> generos;
 	private ArrayList<String> paises;
 	private ArrayList<String> ciudades;
-	
+
 	public DAO() {
 		nombres = new ArrayList<String>();
 		apellidos = new ArrayList<String>();
@@ -18,71 +18,72 @@ public class DAO {
 		generos = new ArrayList<String>();
 		paises = new ArrayList<String>();
 		ciudades = new ArrayList<String>();
-		
+
 	}
-	
+
 	public void agregarNombre(String nombre) {
 		nombres.add(nombre);
 	}
-	
+
 	public void agregarApellido(String apellido) {
 		apellidos.add(apellido);
 	}
-	
+
 	public void agregarEdad(String apellido) {
 		edades.add(apellido);
 	}
-	
+
 	public void agregarGenero(String genero) {
 		generos.add(genero);
 	}
-	
+
 	public void agregarPais(String pais) {
 		paises.add(pais);
 	}
-	
+
 	public void agregarCiudad(String ciudad) {
 		ciudades.add(ciudad);
 	}
-	
+
 	public ArrayList<String> noRepetidosYOrdenados(ArrayList<String> parametro) {
 		ArrayList<String> nomb = new ArrayList<String>();
 		boolean noRepetido = false;
 		int j = 0;
 		nomb.add(parametro.get(0));
-		for (int i = 0; i < nomb.size() ; i++) {
+		for (int i = 0; i < parametro.size() ; i++) {
 			while (noRepetido == false || j < parametro.size()) {
-				if (!nomb.get(i).equals(j)) {
+				if (!nomb.get(i).equals(parametro.get(j))) {
 					noRepetido = true;
 				}
-				else {
-					j++;
-				}
-				
+				j++;	
 			}
+			if(noRepetido) {
+				nomb.add(parametro.get(j-1));
+			}
+			j=0;
+			noRepetido = false;
 		}
 		ordenarAlfabeticamente(nomb);
 		for (String i : nomb) {
-			System.out.println(i);
 		}
 		return nomb;
-		
+
 	}
-	
+
 	private void ordenarAlfabeticamente(ArrayList<String> arreglo) {
 		String auxIn;
-        for (int i = 0; i < arreglo.size(); i++) {
-            auxIn = arreglo.get(i);
-            for (int j = 0 ; j < i ; j++) {
-                if (arreglo.get(j).compareTo(auxIn) <  0 ) {
-                	arreglo.set(i, arreglo.get(j));
-                	arreglo.set(j, auxIn);
-                	auxIn = arreglo.get(i);
-                }
-            }
-        }
-    }
-	
+		for (int i = 0; i < arreglo.size(); i++) {
+			auxIn = arreglo.get(i);
+			for (int j = 0 ; j < i ; j++) {
+				if (arreglo.get(j).compareTo(auxIn) <  0 ) {
+					arreglo.set(i, arreglo.get(j));
+					arreglo.set(j, auxIn);
+					auxIn = arreglo.get(i);
+				}
+			}
+		}
+	}
+
 
 	public ArrayList<String> getNombres() {
 		return nombres;
@@ -131,7 +132,7 @@ public class DAO {
 	public void setCiudades(ArrayList<String> ciudades) {
 		this.ciudades = ciudades;
 	}
-	
-	
+
+
 
 }
