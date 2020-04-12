bf3f62678ad70dc229089f514afe05a89fb25d5a
gabriel <gortega2001@gmail.com>
2020-04-11 19:37:32 -0500

    # WARNING: head commit changed in the meantime
    Merge branch 'ramaDefinitivaAntiJuanito' of
    https://github.com/Gabriel20011/sofiaToxica.git into
    ramaDefinitivaAntiJuanito
    
    Conflicts:
    	Taller2/WebContent/gabox.jsp

--- Taller2/WebContent/Juanito.js
+++ Taller2/WebContent/Juanito.js
@@ -4,7 +4,6 @@ var currentPage = 1;
 var numberPerPage = 50;
 var numberOfPages = 1;
 var contador = 0;
-
 function loadList(arr) {
 	list.push(arr)
 	contador++
@@ -14,30 +13,24 @@ function loadList(arr) {
 	}
 }
 function drawList(arr) {
-	var body = document.getElementsByTagName("body")[0];
-	var tabla   = document.createElement("table");
-	var tblBody = document.createElement("tbody");
-	for (let i = 0; i < arr.length; i++) {
-		var hilera = document.createElement("tr");
-
-		for (let j = 0; j < 6; j++) {
-			var celda = document.createElement("td");
-			var textoCelda = document.createTextNode(arr[i].split(";")[j]);
-			celda.appendChild(textoCelda);
-			hilera.appendChild(celda);
-		}
-		tblBody.appendChild(hilera);
-	}
-
-	tabla.appendChild(tblBody);
-	body.appendChild(tabla);
-	tabla.setAttribute("border", "2");
-
-}
-
-function cargarNPagina(){
-
-	document.getElementById("nPagina").innerHTML = "Pagina: " + currentPage + " de: " + numberOfPages
+			var body = document.getElementsByTagName("body")[0];
+			var tabla   = document.createElement("table");
+			var tblBody = document.createElement("tbody");
+			for (var i = 0; i < arr.length; i++) {
+			    var hilera = document.createElement("tr");
+			 
+			    for (var j = 0; j < 6; j++) {
+			      var celda = document.createElement("td");
+			      var textoCelda = document.createTextNode( arr[i].split(";")[j]);
+			      celda.appendChild(textoCelda);
+			      hilera.appendChild(celda);
+			    }
+			    tblBody.appendChild(hilera);
+			  }
+			 
+			  tabla.appendChild(tblBody);
+			  body.appendChild(tabla);
+			  tabla.setAttribute("border", "2");
 
 }
 
@@ -45,45 +38,27 @@ function cargarListaP(){
 	let inicio = (currentPage - 1) * 50
 	let fin = currentPage * 50
 	var supp1 = []
-	if (list.length > 50){
-		if (fin > list.length){
-			for (inicio ; inicio < list.length ; inicio ++){
-				supp1.push(list[inicio])
-			}
-		}
-		else{
-			for (inicio ; inicio < fin ; inicio ++){
-				supp1.push(list[inicio])
-			}
-		}
-	}
-	else{
-		for (inicio ; inicio < list.length ; inicio ++){
-			supp1.push(list[inicio])
-		}
+	for (inicio ; inicio < fin ; inicio ++){
+		supp1.push(list[inicio])
 	}
 	drawList(supp1)
 }
 function firstPage(){
 	currentPage = 1;
-	cargarListaP()
-	cargarNPagina()
+	cargarListaP();
 }
 function lastPage() {
-	currentPage = numberOfPages;
+	currentPage = numberOfPages-1;
 	cargarListaP();
-	cargarNPagina()
 }
 
 function previousPage() {
 	if (currentPage > 1)
 		currentPage -= 1;
 	cargarListaP();
-	cargarNPagina()
 }
 function nextPage() {
 	if (currentPage < numberOfPages)
 		currentPage += 1;
 	cargarListaP();
-	cargarNPagina()
 }
